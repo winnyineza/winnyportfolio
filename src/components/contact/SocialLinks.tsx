1bd48a2738f2e96b83a8505cb30a552a5a9f2a65
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2025-03-13 15:21:54 +0200

    made changes

--- src/components/contact/SocialLinks.tsx
+++ src/components/contact/SocialLinks.tsx
@@ -9,19 +9,13 @@ const SocialLinks = () => {
       <h2 className="text-2xl font-bold mb-6">SOCIAL INFO</h2>
       <div className="grid grid-cols-3 gap-4">
         <a href="#" className="social-icon">
-          <Facebook className="h-5 w-5" />
-        </a>
-        <a href="#" className="social-icon">
-          <Twitter className="h-5 w-5" />
-        </a>
-        <a href="#" className="social-icon">
-          <Instagram className="h-5 w-5" />
-        </a>
-        <a href="#" className="social-icon">
-          <Phone className="h-5 w-5" />
+          <Behance className="h-5 w-5" />
         </a>
         <a href="#" className="social-icon">
           <Github className="h-5 w-5" />
+        </a>
+        <a href="#" className="social-icon">
+          <Whatsapp className="h-5 w-5" />
         </a>
         <a href="#" className="social-icon">
           <Linkedin className="h-5 w-5" />
